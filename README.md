c953eb934069f5bb49262d910173b812c38ffed5
Jonah Douglas <douglaj8@wwu.edu>
2021-05-25 13:04:20 -0700

    edited bash script to install pip3 in case user doesn't have in order to dowload dependencies

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # RestarauntRESTAPI
 RESTful API that takes order for breakfast, lunch and dinner following specific ruleset given.
+
+Instructions for use:
+1. Download Zip file from github
+2. Unzip file
+3. Navigate into file with bash
+4. Run ./buildproject.sh
+5. If Pip3 is not currently installed, you will need to enter in password for sudo apt install
+
--- buildproject.sh
+++ buildproject.sh
@@ -1,10 +1,10 @@
 # Bash script for prepping project dependencies and starting up programs.
 # Linux environment required.
 # Version: Python 3.6
-pwd
 
 echo -e "Starting to install python flask dependencies...\n\n"
 
+sudo apt install python3-pip
 pip3 install -r Requirements.txt
 
 echo -e "Opening 2nd bash terminal and running test.py..."
@@ -14,5 +14,4 @@ sh ./runTestPy.sh &
 echo -e "\n\nInstallation complete, running main.py...\n\n"
 
 python3 main.py
-
 
